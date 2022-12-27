8a14e80b6628d59785b69dd722cb2f18f5bfad58
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-12-26 20:07:02 -0500

    Working
    Generates a correct spectogram

---
+++ rms.py
@@ -0,0 +1,24 @@
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+y, sr = librosa.load("Monday at 22-31.wav")
+
+rmss = librosa.feature.rms(y=y)
+print("Raw rmss {0}".format(rmss))
+S, phase = librosa.magphase(librosa.stft(y))
+rms = librosa.feature.rms(S=S)
+
+fig, ax = plt.subplots(nrows=2, sharex=True)
+times = librosa.times_like(rms)
+ax[0].semilogy(times, rms[0], label='RMS Energy')
+ax[0].set(xticks=[])
+ax[0].legend()
+ax[0].label_outer()
+librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax[1])
+ax[1].set(title='log Power spectrogram')
+
+S = librosa.magphase(librosa.stft(y, window=np.ones, center=False))[0]
+librosa.feature.rms(S=S)
+plt.show()
