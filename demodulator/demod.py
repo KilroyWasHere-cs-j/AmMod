c7dda24bdf72b6c78794d061243a9ee5f70e1170
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2023-01-17 17:17:51 -0500

    Butterworth filter added
    Added Butterworth filtering

--- demodulator/demod.py
+++ demodulator/demod.py
@@ -1,12 +1,26 @@
 import librosa
 import numpy as np
+from scipy.signal import butter, sosfilt, sosfreqz
+
+filename = "/home/gabetower/Git/AmMod/modulator/path_of_file.wav"
+
+bytes_decoded = 0
+lowpass = 500
+highpass = 1000
+order = 10
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    sos = butter(order, [low, high], analog=False, btype='band', output='sos')
+    return sos
 
 
-filename = "/home/gabrieltower/Documents/GitHub/AmMod/modulator/path_of_file.wav"
-
-sr = librosa.get_samplerate(filename)
-
-stream = librosa.stream(filename, block_length=10, frame_length=10, hop_length=1024)
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    sos = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = sosfilt(sos, data)
+    return y
 
 
 def extract_peak_frequency(data, sampling_rate):
@@ -34,30 +48,48 @@ def decode(bin):
 
 
 #  Who knows how this works Python is shitty
+#  But it breaks the string up into blocks of 8
 def space(string):
     chunk = 8
     return str(' '.join([string[i:i + chunk] for i in range(0, len(string), chunk)]))
 
 
+#  Converts text into binary
 def binToText(bin):
     output = ""
     bytes = bin.split(' ')
     for b in bytes:
+        bi
         output += chr(int(b[:8], 2))
     return output
 
-
-demodulated_text = ""
-
-for frame in stream:
-    peak_freq = int(extract_peak_frequency(frame, sr))
-    if peak_freq == 1000:
-        demodulated_text += "1"
-    elif peak_freq == 500:
-        demodulated_text += "0"
-    elif peak_freq == 750:
-        demodulated_text += "_"
-    else:
-        demodulated_text += ""
+def decodeToText(bin):
+    return binToText(space(decode(bin)))
 
 
+def main(path):
+    sr = librosa.get_samplerate(filename)
+
+    stream = librosa.stream(filename, block_length=10, frame_length=10, hop_length=1024)
+
+    demodulated_text = ""
+
+    for frame in stream:
+        sig = butter_bandpass_filter(frame, lowpass, highpass, sr, order)
+        peak_freq = int(extract_peak_frequency(sig, sr))
+        if peak_freq == 1000:
+            demodulated_text += "1"
+        elif peak_freq == 500:
+            demodulated_text += "0"
+        elif peak_freq == 750:
+            demodulated_text += "_"
+        else:
+            demodulated_text += ""
+        try:
+            print(decodeToText(demodulated_text), end="\r")
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    main(filename)
--- modulator/rms.py
+++ modulator/rms.py
@@ -2,13 +2,32 @@
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import butter, sosfilt, sosfreqz
+
+lowpass = 500
+highpass = 1000
+order = 10
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        sos = butter(order, [low, high], analog=False, btype='band', output='sos')
+        return sos
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+        sos = butter_bandpass(lowcut, highcut, fs, order=order)
+        y = sosfilt(sos, data)
+        return y
 
 y, sr = librosa.load("path_of_file.wav")
 
+y = butter_bandpass_filter(y, lowpass, highpass, sr, order)
 rmss = librosa.feature.rms(y=y)
 print("Raw rmss {0}".format(rmss))
 S, phase = librosa.magphase(librosa.stft(y))
 rms = librosa.feature.rms(S=S)
+
 
 fig, ax = plt.subplots(nrows=2, sharex=True)
 times = librosa.times_like(rms)
